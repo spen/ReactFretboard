c365c70e34f881ca60d216a3f9e87ec4beab41fa
Spen <spen_@hotmail.co.uk>
2020-02-04 13:35:40 +0000

    Generalise OptionSelection component

---
+++ src/components/OptionSelection/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import OptionButton from './OptionButton';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+`;
+
+interface Option {
+    label: string; 
+    value: string;
+}
+
+interface OptionSelectProps {
+    onSelectOption: ( value: string ) => void;
+    options: Option[];
+    selectedOption: any; 
+}
+
+const OptionSelect: React.FC<OptionSelectProps> = ( { options, selectedOption, onSelectOption } ) => (
+    <Container>
+        { 
+            options.map(
+                ( { label, value } ) => (
+                    <OptionButton 
+                        isSelected={ value === selectedOption } 
+                        onClick={ () => onSelectOption( value ) }
+                        key={ `${ label }-${ value }` }
+                    >
+                        { label }
+                    </OptionButton>
+                )
+            ) 
+        }
+    </Container>
+);
+
+export default OptionSelect;
